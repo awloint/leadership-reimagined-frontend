00a16963dc2b7497b8bd95bdac87a53e6fb069c4
Benson Imoh,ST <benson@stbensonimoh.com>
2020-05-05 02:58:13 +0100

    fix: fix issues with API link

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,111 @@
-window.intlTelInputGlobals.loadUtils("scripts/utils.js");
-var input = document.querySelector("#phone");
-window.intlTelInput(input, {
+document.addEventListener('DOMContentLoaded', () => {
+  const date = new Date()
+  const year = date.getFullYear()
+  const yearContent = document.querySelector('#year')
+  yearContent.innerHTML = year
+
+  // Capitalize first letter of First Name and Last Name
+  document.querySelector('#firstName').onchange = e => {
+    let val = document.querySelector('#firstName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#lastName').onchange = e => {
+    let val = document.querySelector('#lastName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#email').onchange = e => {
+    let val = document.querySelector('#email').value
+    RegExp = /\b[a-z]/g
+
+    val = val.toLowerCase()
+  }
+
+  // Add intl-tel-input
+  window.intlTelInputGlobals.loadUtils('scripts/utils.js')
+  var input = document.querySelector('#phone')
+  window.intlTelInput(input, {
     // any initialisation options go here
-    initialCountry: "ng",
+    initialCountry: 'ng',
     separateDialCode: true,
-    hiddenInput: "full_phone",
-    utilsScript: "scripts/utils.js"
-});
+    hiddenInput: 'full_phone',
+    utilsScript: 'scripts/utils.js'
+  })
+
+  //   Submit the form
+  const form = document.querySelector('form')
+
+  // On Form Submit
+  form.addEventListener('submit', e => {
+    // Check to see if form has validation errors
+    if (form.checkValidity() === false) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+
+    // If form doesn't have validation errors
+    if (form.checkValidity() === true) {
+      e.preventDefault()
+
+      // change the button color and add the loading class
+      document.querySelector('button').classList.remove('btn-danger')
+      document.querySelector('button').classList.add('btn-primary')
+      document.querySelector('button').innerHTML =
+        'Loading <span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>'
+
+      // Create Form Variables
+      const firstName = document.querySelector("input[name='firstName'").value
+      const lastName = document.querySelector("input[name='lastName'").value
+      const email = document.querySelector("input[name='email'").value
+      const phone = document.querySelector("input[name='full_phone'").value
+      const city = document.querySelector("input[name='city'").value
+      const country = document.querySelector("select[name='country'").value
+
+      // construct formData
+      const formBody = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phone: phone,
+        city: city,
+        country: country
+      }
+
+      // send it for processing
+      fetch('https://zoom-conference.herokuapp.com/', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          if (data.message === 'Delegate created successfully') {
+            swal(
+              'Registration Successful!',
+              'Your registration was successful! Check your email for the Zoom link',
+              'success'
+            )
+            setTimeout((window.location = 'https://awlo.org'), 5000)
+          } else if (data.message === 'user exists') {
+            swal(
+              'Already Registered!',
+              'You have already registered!',
+              'warning'
+            )
+            setTimeout((window.location = 'https://awlo.org'), 5000)
+          }
+        })
+        .catch(err => {
+          console.log('The request failed', err)
+        })
+    }
+  })
+})
